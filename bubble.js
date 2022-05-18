3f552244a3118e22a0ecd537d91b95bf46f213cf
Buttercup <andyandnile@gmail.com>
2022-05-18 12:30:51 -0700

    added comments with ability to manually test for visualization of changes to array

--- bubble.js
+++ bubble.js
@@ -14,14 +14,24 @@ const bubbleSort = (arr) => {
                 swapEm(arr, j, j+1);
                 swapped = true;
             }
+            //to visualize the changes with each iteration over inner loop during manual test, use below console.log 
+            console.log(`${j+1} time in j`, arr)
         }
+        //to visualize the changes with each iteration over outer loop during manual test, use below console.log 
+        // console.log(`${i+1} time in i`, arr)
         if(!swapped){
+            //to help visualize manual sorted array test, use below console.log
+            // console.log('sorted array', arr)
             break;
         }
     }
     return arr
 }
 
-// console.log(bubbleSort([17, 63, 34, 100, 2, 8, 23]))
+//you can manually test with a sorted array using this console.log
+// console.log(bubbleSort([2, 5, 11, 34, 65, 87, 105]))
+
+//you can manually test with an unsorted array using this console.log
+console.log(bubbleSort([17, 63, 34, 100, 2, 8, 23]))
 
 module.exports = bubbleSort;
